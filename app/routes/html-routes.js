f12a56312485ca48d9cf61d1f01d9106d4f86c62
luvwitch <itsalexkelly@gmail.com>
2020-12-17 17:46:05 -0800

    html error fixes

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -22,17 +22,5 @@ module.exports = function(app) {
   app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/italyhome.html"));
   });
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  app.get("/testing", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/backendTest.html"));
-=======
-  app.get("/favorites", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/favorites.html"));
->>>>>>> ad65815628724c4c531f907d857eb159e7f4e860
-  });
-=======
->>>>>>> 2e4f13cf604edec0bbc33cfee69ddeb74dc6efb5
 
 };
